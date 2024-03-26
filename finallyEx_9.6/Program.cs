7dec96ba5305b82332f332a380d79411ffc2d3ad
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-03-26 09:35:54 +0300

    Добавьте файлы проекта.

---
+++ finallyEx_9.6/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace finallyEx_9._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Exception[] exceptions = new Exception[]
+            {
+                new DivideByZeroException(),
+                new IndexOutOfRangeException(),
+                new ArgumentNullException(),
+                new InvalidOperationException(),
+                new MyExeption("Ошибка в компуктере.")
+            };
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    ThrowException(exception);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Исключение {ex.GetType().Name}: {ex.Message}");
+                }
+                
+            }
+        }
+        static void ThrowException(Exception exception)
+        {
+            // Генерируем исключение
+            throw exception;
+        }
+    }
+    class MyExeption: Exception
+    {
+        public MyExeption()
+        {
+            
+        }
+        public MyExeption(string message): base (message)
+        {
+            
+        }
+    }
+
+}
